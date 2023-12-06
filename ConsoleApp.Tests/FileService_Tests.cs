4404b779736f65e3ee5ef6764eb01e29520bb06b
Edvin <edvin.thornholm@gmail.com>
2023-12-06 12:03:47 +0100

    Test is add to fileservice

---
+++ ConsoleApp.Tests/FileService_Tests.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp.Services;
+using System;
+using Xunit;
+
+namespace ConsoleApp.Tests
+{
+    public class FileService_Tests
+    {
+        [Fact]
+        public void SaveContentToFile_ShouldReturnTrue_WhenContentIsSaved()
+        {
+            // Arrange
+            string filePath = @"C:\Education\C-sharp-assignment\test.txt";
+            string content = "test content";
+
+            IFileService fileService = new FileService(filePath);
+
+            // Act
+            bool result = fileService.SaveContentToFile(content);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
