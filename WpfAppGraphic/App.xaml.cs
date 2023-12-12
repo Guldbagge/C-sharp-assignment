fc230e0f9745b75cc58ac9d167f0b773a904d402
Edvin <edvin.thornholm@gmail.com>
2023-12-12 11:39:08 +0100

    Start

---
+++ WpfAppGraphic/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfAppGraphic
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
