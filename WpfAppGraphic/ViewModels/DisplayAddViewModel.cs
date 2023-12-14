02cf49cdae7039265dcecab506c1ebaeb9317a3e
Edvin <edvin.thornholm@gmail.com>
2023-12-14 13:05:39 +0100

    smal fix

--- WpfAppGraphic/ViewModels/DisplayAddViewModel.cs
+++ WpfAppGraphic/ViewModels/DisplayAddViewModel.cs
@@ -3,53 +3,23 @@ using CommunityToolkit.Mvvm.Input;
 using ConsoleApp.Service;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WpfAppGraphic.ViewModels
 {
-    public partial class DisplayAddViewModel : ObservableObject
+    public partial class DisplayAddViewModel(IServiceProvider sp) : ObservableObject
     {
-        private PersonService _personService;
-        private readonly IServiceProvider _sp;
+        private readonly PersonService _personService = new();
+        private readonly IServiceProvider _sp = sp ?? throw new ArgumentNullException(nameof(sp));
 
-        private string _email;
-        public string Email
-        {
-            get => _email;
-            set
-            {
-                if (_email != value)
-                {
-                    _email = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string StreetName { get; set; }
+        public string Email { get; set; } = "";
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string StreetName { get; set; } = "";
         public int StreetNumber { get; set; }
         public int ZipCode { get; set; }
-        public string City { get; set; }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        public DisplayAddViewModel(IServiceProvider sp)
-        {
-            _sp = sp;
-            _personService = new PersonService();
-        }
+        public string City { get; set; } = "";
 
         [RelayCommand]
         private void NavigateToList()
@@ -72,9 +42,7 @@ namespace WpfAppGraphic.ViewModels
             );
 
             if (newPerson != null)
-            {
                 _personService.AddPerson(newPerson);
-            }
         }
 
         private static Person GetPersonDetailsFromUser(string email, string firstName, string lastName, string streetName, int streetNumber, int zipCode, string city)
