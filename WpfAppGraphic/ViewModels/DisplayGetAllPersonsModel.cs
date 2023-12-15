7c5a95df682873e6b0e306b30b143a2658f65dd4
Edvin <edvin.thornholm@gmail.com>
2023-12-15 07:43:18 +0100

    get all person done

--- WpfAppGraphic/ViewModels/DisplayGetAllPersonsModel.cs
+++ WpfAppGraphic/ViewModels/DisplayGetAllPersonsModel.cs
@@ -1,12 +1,26 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ConsoleApp.Service;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace WpfAppGraphic.ViewModels
 {
     public class DisplayGetAllPersonsModel : ObservableObject
     {
-        public DisplayGetAllPersonsModel()
+        private readonly PersonService _personService = new();
+        public ObservableCollection<string> DisplayedPersons { get; } = new ObservableCollection<string>();
+
+        public async Task GetAllPersonsAsync()
         {
-            // Här kan du lägga till kod för att initialisera några tomma värden eller ladda data från en tjänst eller databas.
+            var persons = await Task.Run(() => _personService.GetPersons());
+            DisplayedPersons.Clear();
+            foreach (var person in persons)
+            {
+                DisplayedPersons.Add($"{person.FirstName} {person.LastName}");
+                DisplayedPersons.Add("----------------------------------");
+            }
         }
     }
 }
--- WpfAppGraphic/Views/DisplayGetAllPersons.xaml.cs
+++ WpfAppGraphic/Views/DisplayGetAllPersons.xaml.cs
@@ -12,17 +12,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAppGraphic.ViewModels;
+
+//namespace WpfAppGraphic.Views
+//{
+//    /// <summary>
+//    /// Interaction logic for DisplayGetAllPersons.xaml
+//    /// </summary>
+//    public partial class DisplayGetAllPersons : UserControl
+//    {
+//        public DisplayGetAllPersons()
+//        {
+//            InitializeComponent();
+//        }
+//    }
+//}
+
 
 namespace WpfAppGraphic.Views
 {
-    /// <summary>
-    /// Interaction logic for DisplayGetAllPersons.xaml
-    /// </summary>
     public partial class DisplayGetAllPersons : UserControl
     {
+        private readonly DisplayGetAllPersonsModel _viewModel;
+
         public DisplayGetAllPersons()
         {
             InitializeComponent();
+            _viewModel = new DisplayGetAllPersonsModel();
+            DataContext = _viewModel;
+
+            Loaded += async (_, __) => await _viewModel.GetAllPersonsAsync();
         }
     }
 }
