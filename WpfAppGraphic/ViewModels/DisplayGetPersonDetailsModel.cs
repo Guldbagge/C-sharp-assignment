1b8b17510540f612cfbd9203fa41cecd5d961836
Edvin <edvin.thornholm@gmail.com>
2023-12-15 12:15:16 +0100

    GetPersonDetails Done and work

--- WpfAppGraphic/ViewModels/DisplayGetPersonDetailsModel.cs
+++ WpfAppGraphic/ViewModels/DisplayGetPersonDetailsModel.cs
@@ -1,9 +1,75 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ConsoleApp.Service;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows.Input;
 
 namespace WpfAppGraphic.ViewModels
 {
-    public class DisplayGetPersonDetailsModel : ObservableObject
+    public partial class DisplayGetPersonDetailsModel : ObservableObject
     {
-        
+        private readonly PersonService _personService;
+        private readonly IServiceProvider _sp;
+
+        public DisplayGetPersonDetailsModel(IServiceProvider sp)
+        {
+            _sp = sp ?? throw new ArgumentNullException(nameof(sp));
+            _personService = new PersonService(); // Kan injiceras genom konstruktorn om den är registrerad i tjänsterna.
+            NavigateToListCommand = new RelayCommand(NavigateToList);
+            GetPersonDetailsCommand = new RelayCommand(GetPersonDetails);
+        }
+
+        public ICommand NavigateToListCommand { get; }
+        public ICommand GetPersonDetailsCommand { get; }
+
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set => SetProperty(ref _email, value);
+        }
+
+        private string _personDetails;
+        public string PersonDetails
+        {
+            get => _personDetails;
+            set => SetProperty(ref _personDetails, value);
+        }
+
+        private void NavigateToList()
+        {
+            var mainViewModel = _sp.GetRequiredService<MainViewModel>();
+            mainViewModel.CurrentViewModel = _sp.GetRequiredService<DisplayMainOptionsViewModel>();
+        }
+
+        private void GetPersonDetails()
+        {
+            string emailInput = Email;
+
+            var result = _personService.GetPerson(emailInput);
+
+            if (result != null)
+            {
+                DisplayPersonInformation(result);
+            }
+            else
+            {
+                PersonDetails = "Person not found.";
+            }
+        }
+
+        private void DisplayPersonInformation(Person person)
+        {
+            PersonDetails = $"\nPerson Information:\n----------------------------------\n" +
+                            $"Email: {person.Email}\n" +
+                            $"First Name: {person.FirstName}\n" +
+                            $"Last Name: {person.LastName}\n" +
+                            $"Address:\n\tStreet Name: {person.StreetName}\n" +
+                            $"\tStreet Number: {person.StreetNumber}\n" +
+                            $"\tZip Code: {person.ZipCode}\n" +
+                            $"\tCity: {person.City}\n" +
+                            $"----------------------------------\n\n";
+        }
     }
 }
